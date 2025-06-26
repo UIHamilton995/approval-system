1ab96cd3b7ee12a0a1be4475599ae6c50f921f17
Hamilton <dehamiltonglobal@gmail.com>
2025-06-26 22:47:36 +0100

    searchProvider

--- src/contexts/SearchContext.jsx
+++ src/contexts/SearchContext.jsx
@@ -3,4 +3,19 @@ import { createContext, useContext, useState } from "react";
 // create the context by the advantage of create context
 const SearchContext = createContext()
 
-export const
+export const useSearch = () => {
+  const context = useContext(SearchContext)
+  if (!context){
+    throw new Error("useSearch must be used within a searchProvider")
+  }return context
+};
+
+export const SearchProvider = ( children ) => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  return (
+    <SearchContext.Provider value={{ searchTerm, setSearchTerm}}>
+      {children}
+    </SearchContext.Provider>
+  )
+};
